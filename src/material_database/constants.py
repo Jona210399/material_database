fba3ce260c5fef106fda218470f552fc0ee6888e
Jona Östreicher <jona.oestreicher@kit.edu>
2025-09-30 11:19:18 +0200

    create rdsim module

--- src/material_database/constants.py
+++ src/material_database/constants.py
@@ -2,3 +2,6 @@ class ColumnNames:
     ID = "id"
     SYMMETRIZED_STRUCTURE = "symmetrized_structure"
     CIF = "cif"
+
+    PEAK_TWO_THETAS = "peak_two_thetas"
+    PEAK_INTENSITIES = "peak_intensities"
